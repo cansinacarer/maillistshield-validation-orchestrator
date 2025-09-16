6fa75de6f0247d1909f12a58799adc28bcfa881c
Cansin Acarer <cansinacarer@users.noreply.github.com>
2025-09-16 01:05:48 -0400

    feat: add pausing and polling interval

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import json
+import time
 
 from app.utilities.rabbitmq import QueueAgent
 from app.utilities.logging import logger
-from app.config import ROWS_PER_ROUND
+from app.config import ROWS_PER_ROUND, POLLING_INTERVAL, PAUSE
 from app.process_email import EmailProcessor
 
 queue_agent = QueueAgent()
@@ -10,9 +11,21 @@ email_processor = EmailProcessor()
 
 
 while True:
+    # Pause if env variable is set to pause
+    if PAUSE:
+        logger.info(
+            "File to validation queue publisher is paused, change the environment variable `PAUSE` to resume it."
+        )
+        time.sleep(POLLING_INTERVAL)
+        continue
+
+    # Iterations start time
+    start_time = time.time()
+
     discovered_queues = queue_agent.list_all_queues()
     if len(discovered_queues) == 0:
-        logger.debug("No queues found. Retrying...")
+        logger.debug(f"No queues found. Sleeping for {POLLING_INTERVAL} seconds.")
+        time.sleep(POLLING_INTERVAL)
         continue
     logger.debug(f"Discovered Queues: {discovered_queues}")
 
@@ -41,3 +54,11 @@ while True:
                 logger.debug(
                     f"Round of processing {ROWS_PER_ROUND} messages from all queues is complete."
                 )
+
+        # Iteration end time
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        # If the elapsed time is not as long as the polling interval, sleep until it is
+        sleep_time = POLLING_INTERVAL - elapsed_time
+        if sleep_time > 0:
+            time.sleep(sleep_time)
